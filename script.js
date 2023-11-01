9c50967744f431085ac9f32b74f2037cfc74d28a
Rahul Sedha <125468426+Rahulsedha@users.noreply.github.com>
2023-11-01 18:55:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ ScrollReveal().reveal('.home-content h1 , .about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p , .about-content',{origin:'right'});
 
 const typed=new Typed('.multiple-text',{
-    strings:['Software Developer','Web Designer','Photographer'],
+    strings:['Software Developer','Web Designer','DBA Administrator'],
     typeSpeed:100 ,
     backSpeed:100,
     backDelay:1000,
