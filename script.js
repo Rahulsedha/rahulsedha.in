51b32e69a414e075e51510a6bcf87e454a9fbf24
Rahul Sedha <125468426+Rahulsedha@users.noreply.github.com>
2024-05-26 10:00:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ ScrollReveal().reveal('.home-content h1 , .about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p , .about-content',{origin:'right'});
 
 const typed=new Typed('.multiple-text',{
-    strings:['Web Developer','Software Developer','DB Administrator'],
+    strings:['Web Designer','DB Administrator'],
     typeSpeed:100 ,
     backSpeed:100,
     backDelay:1000,
